4bd57e1b6aea8a70fc82e80233e6a636de23cb94
Dreyzyy <Andreygcm2000@gmail.com>
2025-09-22 18:47:41 -0300

    função do botão

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('apenas pra salvar');
+daviBrito()
+function daviBrito () {
+let botaoUm = document.getElementById('botao1');
+let containerImagem = document.getElementById('containerDaImagem');
+
+botaoUm.addEventListener('click', () => {
+    const imagem = document.createElement('img');
+    imagem.src = 'davi-brito-1706289291.png';
+    containerImagem.appendChild(imagem);
+})
+}
