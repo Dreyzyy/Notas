5fb60d52b5286f86ef517aeba0f6283226d51190
Matheus-kv <matheus.victorkv@gmail.com>
2025-09-26 14:25:15 -0300

    conserto de titulo

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 let notasID = document.getElementById('notas');
 notasID.addEventListener('input', armazenamentoLocal) ;
     
-/* let tituloID = document.getElementById('areaDoTítulo');
+ let tituloID = document.getElementById('areaDoTítulo');
 tituloID.addEventListener('input', armazenamentoLocal) 
 
-INTERDITADO ATÉ TIRAR O FORM DO TITULO  
-
- tituloID.value = localStorage.getItem('Titulo') || ''; */
+ tituloID.value = localStorage.getItem('Titulo') || '';
  notasID.value = localStorage.getItem('notas') || '';
    
 
 
 function armazenamentoLocal() {
-    /* let titulo = tituloID.value 
-    
-    INTERDITADO ATÉ TIRAR O FORM DO TITULO
+
+    let titulo = tituloID.value 
     
     let armazemTitulo = localStorage.setItem('Titulo', titulo);
-     */
+     
     let notas = notasID.value;
+    
     let armazemNota = localStorage.setItem('notas', notas);
 }
 
